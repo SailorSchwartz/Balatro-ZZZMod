9efbe5018ec303aa292dbd093a41cc01b581e897
Sailor <sailorschwartz74@gmail.com>
2026-08-20 20:09:09 +0100

    more base files

--- main.lua
+++ main.lua
@@ -2,3 +2,4 @@ local jokers_src = SMODS.NFS.getDirectoryItems(SMODS.current_mod.path .. "src/jo
 for _, file in ipairs(jokers_src) do
     assert(SMODS.load_file("src/jokers/" .. file))()
 end
+-- looks through joker folder and loads every file
